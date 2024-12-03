58d6c00f7bd4f8e19b97697d3b91e6c71f65f383
Ahmed Elesawy <56431746+ahmedyosry433@users.noreply.github.com>
2024-12-03 16:47:37 +0200

    add base

--- js/main.js
+++ js/main.js
@@ -1,22 +1,33 @@
-let showRecipes = async () => {
-    try {
-        const url = "https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886";
-    let res = await fetch(
-        url,
-        {
-            method: "GET",
-        }
-    );
-        let data = await res.json();
-        console.log(data.data.recipe);
+// let showRecipes = async () => {
+//     try {
+//         const url = "https://forkify-api.herokuapp.com/api/v2/recipes?search=pizza";
+//     let res = await fetch(
+//         url,
+//         {
+//             method: "GET",
+//         }
+//     );
+//         let data = await res.json();
+//         console.log(data.data.recipe);
 
 
-        if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+//         if (!res.ok) throw new Error(`${data.message} (${res.status})`);
 
-    }catch(err){
-        alert(err.message);
-   }
+//         let { recipe } = data.data.recipe;
+//         recipe = {
+//             id: recipe.id,
+//             title: recipe.title,
+//             image: recipe.image_url,
+//             ingredients: recipe.ingredients,
+//             servings: recipe.servings,
+//             cooking_time: recipe.readyInMinutes,
+//             instructions: recipe.instructions
+//         }
+
+//     }catch(err){
+//         alert(err.message);
+//    }
     
-}
+// }
 
-showRecipes();
+// showRecipes();
